7e629d3b910ba66aa24b343981ebb6e941fffa8a
Scissors <>
2024-12-17 23:22:39 +0300

    [DEV] Project created

---
+++ ProjectB/ProjectB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectB
+//
+//  Created by Scissors on 17.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
